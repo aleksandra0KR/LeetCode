35255ac5980715f513e74dc99619592056292d4f
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2023-09-20 20:17:06 +0300

    Time: 4 ms (20.68%), Space: 4.4 MB (56.15%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+        return helper(1, n)
+    }
+
+    func helper(left, right int)[]*TreeNode{
+        var res []*TreeNode
+
+        if(left > right) {
+            res = append(res, nil)
+            return res
+        }
+
+        if(left == right){
+            currentTree := &TreeNode{Val: left, Left: nil, Right: nil}
+            res = append(res, currentTree)
+            return res
+        }
+
+        for current := left; current <= right; current++{
+            l := helper(left, current - 1)
+            r := helper(current + 1, right)
+            for i := 0; i < len(l); i++{
+                for j := 0; j < len(r); j++{
+                    currentTree := &TreeNode{Val: current, Left: l[i], Right: r[j]}
+                    res = append(res, currentTree)
+                }
+            }
+        }
+        return res
+        
+    }
