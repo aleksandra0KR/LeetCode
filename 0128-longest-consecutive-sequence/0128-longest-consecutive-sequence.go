95db8734b5cc7ab16eff91745eb2d8a9c5094b43
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-01-19 23:01:57 +0300

    Time: 90 ms (86.82%), Space: 9 MB (92.87%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
@@ -0,0 +1,29 @@
+func longestConsecutive(nums []int) int {
+    if len(nums) < 1{
+        return 0
+    } else if len(nums) == 1{
+        return 1
+    } 
+    
+    sort.Ints(nums)
+        
+    maxConsequence := 0
+    current := 0
+        
+    for i := 0; i < len(nums) - 1; i++ {
+        if nums[i] == nums[i+1] - 1{
+            current++
+        } else if nums[i] == nums[i+1]{
+            continue
+        } else{
+            maxConsequence = max(current, maxConsequence)
+            current = 0
+        }
+    
+    }
+    
+    maxConsequence = max(current, maxConsequence) + 1
+
+        
+    return maxConsequence
+}
