a753f7123f3dc87a0502f455bac24a8598380157
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-02-28 22:30:01 +0300

    Time: 31 ms (46.91%), Space: 7.7 MB (81.13%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.go
@@ -0,0 +1,31 @@
+func minSubArrayLen(target int, nums []int) int {
+    sum := 0
+    left := 0
+    right := 0
+    numsLenght := len(nums)
+    resLenght := 1000000
+        
+    for  right < numsLenght {
+            
+        if sum < target {
+            sum += nums[right]
+            right++
+                
+        } else{
+            resLenght = min(resLenght, right - left);
+            sum -= nums[left]
+            left++
+        }
+            
+    }
+        for  sum >= target {
+            resLenght = min(resLenght, right - left)
+            sum -= nums[left]
+            left++
+        }
+        
+    if(resLenght == 1000000) {
+        return 0
+    }
+    return resLenght
+}
