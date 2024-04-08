c36ca5b16348e9fc085d8ccdf712fe83c95b0b5f
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-04-08 20:40:15 +0300

    Time: 11 ms (60.35%), Space: 6 MB (86.62%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,19 +1,17 @@
 func twoSum(numbers []int, target int) []int {
-    var left int = 0
+    
     var right int = len(numbers) - 1
  
-    for left < right {
-        if numbers[right] + numbers[left] > target{
+    for left := 0; left < len(numbers); left++ {
+        for numbers[right] + numbers[left] > target{
             right--
         }
-        if numbers[right] + numbers[left] < target{
-            left++
-        }
         if numbers[right] + numbers[left] == target{
-            break
+            result := []int{left + 1, right + 1}
+            return result
         }
     }
+     return []int{}
     
-    return []int{left + 1, right + 1}
     
 }
