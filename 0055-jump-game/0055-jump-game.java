96ee2cdcf590732a0ba8f059c2e6aa32d5485ea7
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-04-22 22:56:20 +0300

    Time: 2 ms (81.97%), Space: 45.7 MB (39.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,13 @@
-func canJump(nums []int) bool {
-    
-    maxPosition := 0
-
-    for i:= 0; i < len(nums); i++ {
-        if maxPosition >= len(nums) - 1 {
-            return true
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxPosition = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(maxPosition >= nums.length - 1) return true;
+            if(nums[i] == 0 && maxPosition == i) return false;
+            if(nums[i] + i > maxPosition) maxPosition = nums[i] + i ;
         }
-        if nums[i] == 0 && maxPosition == i {
-            return false;
-        }
-        if nums[i] + i > maxPosition {
-            maxPosition = nums[i] + i
-        }
+        
+        return true;
     }
-    
-    return true
 }
