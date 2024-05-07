0a9f0fb958a685eb57d5c7c217f11ab091650835
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-05-07 20:08:58 +0300

    Time: 2 ms (78.78%), Space: 2.5 MB (57.00%) - LeetHub

--- 0274-h-index/0274-h-index.go
+++ 0274-h-index/0274-h-index.go
@@ -1,20 +1,16 @@
 func hIndex(citations []int) int {
-    all := make([]int, 10000)
-    maxValue := 0
+    sort.Slice(citations, func(i, j int) bool {
+        return citations[i] > citations[j]
+    } )
     
-    for i:= 0; i < len(citations); i++ {
-        for citations[i] - 1 >= 0 {
-            citations[i]--
-            all[citations[i]]++
+    result := 0
+    for i := 0; i < len(citations); i++{
+        if i + 1 <= citations[i] {
+            result = i + 1
+        } else {
+            return result
         }
     }
     
-    for i:= 0; i < len(all); i++ {
-        if all[i] >= i + 1 {
-            maxValue = i + 1
-        }
-    }
-    
-    
-    return maxValue
+    return result
 }
