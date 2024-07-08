ab315d11a9eae782ee3ac11f669c1f249728e04c
Aleksandra Kriuchkova <114208185+aleksandra0KR@users.noreply.github.com>
2024-07-08 12:23:18 +0300

    Time: 5 ms (81.55%), Space: 5.6 MB (22.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.go
@@ -8,7 +8,7 @@
 
 
 func mergeKLists(lists []*ListNode) *ListNode {
-    array := make ([]int, 0, 500 )
+    array := make ([]int, 0, 50)
     
     for i := 0; i < len(lists); i++ {
         j := lists[i]
